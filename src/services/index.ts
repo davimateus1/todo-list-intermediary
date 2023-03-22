1741819241a374fc0d372f42a7d9598d90630a70
Davi Mateus <davi.2020109493@unicap.br>
2023-03-21 23:26:21 -0300

    feat: create .env and .env.example

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
 const getUsername = JSON.parse(localStorage.getItem('@user') || '{}');
-
+console.log(import.meta.env.VITE_LOCAL_API_URL, 'AQ');
 export const api = axios.create({
-  baseURL: 'http://localhost:3333',
+  baseURL: import.meta.env.VITE_LOCAL_API_URL,
   headers: {
     username: getUsername.username,
   },
